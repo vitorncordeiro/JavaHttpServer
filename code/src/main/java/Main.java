4f8f95b8180402633f3279d62a8ed63ae1b31bc2
vitorncordeiro <vitornc31@gmail.com>
2026-03-18 20:21:36 -0300

    feat: add the minimal viable TCP server with serverSocket e Socket client connection

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Selecione uma porta para inicializar o servidor: ");
+        int port = scanner.nextInt();
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server ouvindo na porta " + port);
+
+        Socket socket = serverSocket.accept();
+
+        BufferedReader entrada = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
+        PrintWriter saida = new PrintWriter(
+                socket.getOutputStream(), true);
+
+        String linha;
+        while((linha = entrada.readLine()) != null){
+            System.out.println("in: " + linha);
+            saida.println(scanner.nextLine());
+        }
+
+        socket.close();
+        serverSocket.close();
     }
 }
