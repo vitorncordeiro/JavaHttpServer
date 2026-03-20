611d6b3b7dd9713e25334998b36b13588fb483c1
vitorncordeiro <vitornc31@gmail.com>
2026-03-20 17:30:18 -0300

    feat: adds HTTP parsing code

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -1,31 +1,53 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Selecione uma porta para inicializar o servidor: ");
-        int port = scanner.nextInt();
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server ouvindo na porta " + port);
+        ServerSocket serverSocket = new ServerSocket(8080);
 
         Socket socket = serverSocket.accept();
 
-        BufferedReader entrada = new BufferedReader(
+        BufferedReader reader = new BufferedReader(
                 new InputStreamReader(socket.getInputStream())
         );
-        PrintWriter saida = new PrintWriter(
-                socket.getOutputStream(), true);
 
-        String linha;
-        while((linha = entrada.readLine()) != null){
-            System.out.println("in: " + linha);
-            saida.println(scanner.nextLine());
+        String requestLine = reader.readLine();
+        String[] parts = requestLine.split(" ");
+        String method = parts[0];
+        String path = parts[1];
+        String version = parts[2];
+
+        Map<String, String> headers = new HashMap<>();
+        String headerLine;
+        while(!(headerLine = requestLine).isEmpty()){
+            String[] kv = headerLine.split(": ", 2);
+            headers.put(kv[0], kv[1]);
         }
+
+        if(method.equals("POST")){
+            int length = Integer.parseInt(headers.get("Content-Type"));
+            InputStream inputStream = socket.getInputStream();
+
+            byte[] body = new byte[length];
+
+            int totalRead = 0;
+
+            while(totalRead < length){
+                int readed = inputStream.read(body, totalRead, length - totalRead);
+                if(readed == -1) break;
+                totalRead += readed;
+            }
+
+            String bodyStr = new String(body, StandardCharsets.UTF_8);
+        }
+
 
         socket.close();
         serverSocket.close();
